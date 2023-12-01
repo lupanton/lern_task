617c591abfdbeef8b2fee165bd87caa775c5000d
DESKTOP-K8ICI0V\alupanov <lupanov_ae@transset.ru>
2023-12-01 22:04:13 +0300

    db with sqlalchemy added

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,15 @@
 from flask import Flask
+from config import Config
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
-from app import routes
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+app.app_context().push()
+from app import routes, models
+bootstrap = Bootstrap(app)
+
+if __name__ == "__main__":
+    with app.app_context():
+        app.run(debug=True)
